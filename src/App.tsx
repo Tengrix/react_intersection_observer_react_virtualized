ae4c446c4a141390e56a77d3163ef71e4fea2646
Tengrix <damir861@gmail.com>
2023-09-24 20:25:28 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useInView} from "react-intersection-observer";
+import useFetch, {AlbumType} from "./useFetch";
+import {useEffect, useState} from "react";
+import {List, AutoSizer, CellMeasurer, CellMeasurerCache, ListRowRenderer} from 'react-virtualized';
+import './App.css'
+
+const rowHeight = 50;
+const cache = new CellMeasurerCache({
+    fixedWidth: true,
+    defaultHeight: 100
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {ref, inView} = useInView({
+        threshold: 1
+    })
+    const [data, setData] = useState<AlbumType[]>([])
+    const [page, setPage] = useState<number>(1)
+
+    const {albums} = useFetch('https://jsonplaceholder.typicode.com/posts', page)
+    useEffect(() => {
+        if (albums) {
+            setData((prev) => [...prev, ...albums])
+        }
+    }, [albums])
+    useEffect(() => {
+        if (inView && albums.length > 0) {
+            setPage((prev) => prev + 1)
+        }
+    }, [inView])
+
+    const renderRow: ListRowRenderer = ({index, key, style,parent}) => {
+        console.log(style)
+        return (
+            <CellMeasurer
+                key={key}
+                cache={cache}
+                parent={parent}
+                columnIndex={0}
+                rowIndex={index}>
+                {({registerChild}) => (
+                    <div key={key} style={style} className="row" ref={registerChild as any}>
+                        <div className="image">
+                            <img src={data[index].url} alt=""/>
+                        </div>
+                        <div className="content">
+                            <div>{data[index].id}</div>
+                            <div>{data[index].title}</div>
+                        </div>
+                        {index === data.length - 1 && <div ref={ref}/>}
+                    </div>
+                )}
+            </CellMeasurer>
+        )
+    }
+    return (
+        <div className="App">
+            <div className="list">
+                <AutoSizer>
+                    {({width, height}) =>{
+                        // console.log(width,height)
+                        return <List
+                            width={width}
+                            height={height}
+                            deferredMeasurementCache={cache}
+                            rowHeight={rowHeight}
+                            rowRenderer={renderRow}
+                            rowCount={data.length}
+                            overscanRowCount={3}/>
+                    }}
+                </AutoSizer>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+import axios from "axios";
+type ResponseType = {
+    data:AlbumType[]
+}
+export type AlbumType = {
+    albumId: number;
+    id: number;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
+}
 
-const UseFetch = () => {
-    return (
-        <div>
-            
-            </div>
-    );
+interface State {
+    albums: AlbumType[]
+    isLoading: boolean
+    isError: Error | null
+}
+
+const UseFetch = (url: string, _page: number):State => {
+    const [albums, setAlbums] = useState<AlbumType[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [isError, setIsError] = useState(null)
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const posts = await axios.get<void, ResponseType>(url, {
+                    params: {
+                        _page,
+                        _limit: 20
+                    }
+                })
+                setAlbums(posts.data)
+                setIsLoading(false)
+            } catch (e) {
+                setIsError(e as any)
+            }
+        }
+        getPosts()
+    }, [url, _page])
+    return {albums, isLoading, isError};
 };
 
 export default UseFetch;
